fa5a032f165431adfffa9fd3d04d092a7e4cbb1f
Bart Dority <b0rgBart3@gmail.com>
2022-08-25 08:48:28 -0700

    initial commit

---
+++ Tiptime/Tiptime/TiptimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiptimeApp.swift
+//  Tiptime
+//
+//  Created by Bart Dority on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TiptimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
